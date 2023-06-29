71fd8123165b5edaed92e1e07e291f73ba123503
OlhaZam <137802329+OlhaZam@users.noreply.github.com>
2023-06-29 14:59:29 -0400

    #5

--- #5-Week-2-Project.sql
+++ #5-Week-2-Project.sql
@@ -1,8 +1,7 @@
 /*Show all customers who has payments in February*/
 
-WITH customer AS (
-	SELECT customer.customer_id, customer.first_name, last_name FROM customer),
-	month AS (SELECT DISTINCT EXTRACT(MONTH FROM payment.payment_date) AS payment_month FROM payment
-			 WHERE EXTRACT(MONTH FROM payment.payment_date)=2)
+WITH customer AS (SELECT customer.customer_id, customer.first_name, last_name FROM customer),
+	p_month AS (SELECT DISTINCT EXTRACT(MONTH FROM payment.payment_date) AS payment_month FROM payment
+			WHERE EXTRACT(MONTH FROM payment.payment_date)=2)
 SELECT * FROM customer
-NATURAL JOIN month;
+NATURAL JOIN p_month;
